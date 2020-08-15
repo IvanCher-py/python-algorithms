dd338831ea814f84c8f067ac058068a5fe3ab385
IvanCher-py <62182708+IvanCher-py@users.noreply.github.com>
2020-08-15 18:53:08 +0300

    Add files via upload

---
+++ Lesson-3/task_7.py
@@ -0,0 +1,21 @@
+# Задание № 7 - В одномерном массиве целых чисел определить два наименьших элемента.
+# Они могут быть как равны между собой (оба являться минимальными), так и различаться.
+
+import random
+
+SIZE = 10
+MIN_ITEM = -10
+MAX_ITEM = 10
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+
+a = 0
+b = 0
+for x in array:
+    if x <= a:
+        a = x
+        b = a
+    elif x < b:
+        b = x
+
+print(a, b)
